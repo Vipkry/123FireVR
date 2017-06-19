4ae955189a64c3cfbb87c7cf3769f812afa69252
vipkry <lolpsxvipkry2@gmail.com>
2017-06-19 11:25:11 -0300

    done smshts

--- Assets/123Fire Assets/Scripts/Menu/Menu.cs
+++ Assets/123Fire Assets/Scripts/Menu/Menu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
 
+	public float lookDelayTime = 30;
+	private float currentTime = 0;
+	private bool isLooking = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,12 +16,28 @@ public class Menu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (isLooking){
+			currentTime++;
+			Debug.Log (currentTime);
+		}
+
+		if (currentTime >= lookDelayTime){
+			NewGame ();
+		}
 	}
 
 
 	public void NewGame (){
 		Debug.Log ("entered");
 		SceneManager.LoadScene ("mainGame");
+	}
+
+	public void onReticleEnter(){
+		isLooking = true;
+	}
+
+	public void onReticleExit (){
+		currentTime = 0;
+		isLooking = false;
 	}
 }
