f4bafc299231c8cd89a942f6bda1ba443b4ec048
Maifriende <luciano.plouvier@hotmail.com>
2017-06-30 08:00:21 -0300

    EnemySpawner, Pathing
    Custom movement needed.

---
+++ Assets/123Fire Assets/Scripts/SimpleSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSpawner : MonoBehaviour {
+
+    // Use this for initialization
+
+    public int spawnedEnemies;
+    private float spawnDowntime;
+    int[] waveSize = { 2, 3 };
+    protected WaypointManager myWaypoint;
+
+    public GameObject enemyPrefab;
+    public Transform spawnPoint;
+    
+	void Start () {
+        StartCoroutine(spawnCoroutine());
+        spawnedEnemies = 0;
+        spawnDowntime = 3.0f;
+        myWaypoint = GetComponent<WaypointManager>();
+	}
+
+    IEnumerator spawnCoroutine()
+    {
+        while (true)
+        {
+            do
+            {
+                yield return new WaitForSecondsRealtime(spawnDowntime);
+            } while (spawnedEnemies >= 10);
+
+            myWaypoint.AddEntity((GameObject)Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation));
+            spawnedEnemies = spawnedEnemies + 1;
+        
+        }
+
+    }
+
+}
