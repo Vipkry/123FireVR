b56f46495f1347686d8ba240213d682c8487fb87
vipkry <lolpsxvipkry2@gmail.com>
2017-07-01 14:01:41 -0300

    small changes upon shooting and added bgm

---
+++ Assets/AudioFadeIn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFadeIn : MonoBehaviour {
+
+	public float steps = 0.01f;
+	public float maxVolume = 0.3f;
+	private AudioSource myAudioSource;
+
+	// Use this for initialization
+	void Start () {
+		myAudioSource = GetComponent<AudioSource> ();
+		myAudioSource.volume = 0.01f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (myAudioSource.volume < maxVolume){
+			myAudioSource.volume = myAudioSource.volume + steps;	
+		}
+
+	}
+}
