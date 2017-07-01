4c7f849dafed6ca616d076f1f4fb5f92e20f8326
vipkry <lolpsxvipkry2@gmail.com>
2017-07-01 18:54:54 -0300

    raycast implemented, fixing some issues'

--- Assets/123Fire Assets/Scripts/Shoot.cs
+++ Assets/123Fire Assets/Scripts/Shoot.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour {
 	
-	public float damage = 10f;
+
 	public ParticleSystem rightParticleSystem;
 	public ParticleSystem leftParticleSystem;
 	private bool isLeftShooting;
+
+	public float damage = 10f;
+	public float blastRadius = 5f;
+	public Transform crosshairTransform;
+	public LayerMask enemyLayer;
 
 	public AudioSource shootSoundSource;
 	public AudioClip shootSoundClip;
@@ -17,11 +22,11 @@ public class Shoot : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		if (Input.GetMouseButtonDown(0) && actualDelayTime >= delayTime){
 			if (isLeftShooting){
 				leftParticleSystem.Play();
@@ -33,17 +38,39 @@ public class Shoot : MonoBehaviour {
 				shootSoundSource.panStereo = 0.3f;
 				isLeftShooting = true;
 			}
+			castShooting ();
 			actualDelayTime = 0;
 			shootSoundSource.PlayOneShot (shootSoundClip);
-				
 		}
-
 	}
 
 	void FixedUpdate (){
 		if (actualDelayTime < delayTime)
 			actualDelayTime++;
 	}
+
+	private void castShooting(){
+
+		Vector3 cameraPos = Camera.main.transform.position;
+		Vector3 dir = crosshairTransform.position - cameraPos;
+		Debug.DrawRay(cameraPos, dir * 100,  Color.red, 5);
+
+		RaycastHit hit;
+		Ray ray = new Ray (cameraPos, dir);
+
+
+		if (Physics.Raycast(ray, out hit, 1000f)){
+			// 9 é a layer dos inimigos
+			Collider[] collisions = Physics.OverlapSphere (hit.point, blastRadius, enemyLayer.value);
+			if (collisions.Length > 0){
+				Debug.Log (collisions.Length + " targets hit!");
+			}
+
+		}
+
+	}
+
+
 
 
 }
